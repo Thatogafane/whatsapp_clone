02655ab70d7e1d6fbe60a5b06b313b9274752f38
ThatoGafane <thato@connectkasi.co.za>
2021-02-17 00:27:34 +0200

    created appbar

---
+++ lib/constant.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF101619);
+const kSecondaryColor = Color(0xFF1A6A5E);
+const kAppBarColor = Color(0xFF262A2B);
+const kTextColor = Color(0xFF999EA5);
---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/constant.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: false,
+            title: const Text(
+              'WhatsApp',
+              // style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            backgroundColor: kAppBarColor,
+            elevation: 0,
+            actions: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(right: 20.0),
+                child: Icon(Icons.search),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 20.0),
+                child: Icon(Icons.more_vert),
+              ),
+            ],
+            bottom: TabBar(
+              labelStyle: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.camera_alt),
+                ),
+                Tab(
+                  child: Text('CHAT'),
+                ),
+                Tab(
+                  child: Text('STATUS'),
+                ),
+                Tab(
+                  child: Text('CALLS'),
+                ),
+              ],
+              indicatorColor: kSecondaryColor,
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              Icon(Icons.camera),
+              Text('Chat Screen'),
+              Text('Status Screen'),
+              Text('Call Screen'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
